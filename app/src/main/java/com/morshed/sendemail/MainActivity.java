589227ac72a24cbcfa8c355ed94987bfe6a408a1
shawonswe1 <shawon.swe1@gmail.com>
2021-02-06 12:21:13 +0600

    Initial commit

---
+++ app/src/main/java/com/morshed/sendemail/MainActivity.java
@@ -0,0 +1,43 @@
+package com.morshed.sendemail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText email, subject, message;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        subject = findViewById(R.id.subject);
+        message = findViewById(R.id.message);
+        button = findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                senEmail();
+            }
+        });
+    }
+
+    private void senEmail() {
+        String mEmail ="shubho.techdynobd@gmail.com";
+        String mSubject = "Receive Order";
+        String mMessage = "You receive another Order";
+
+
+        JavaMailAPI javaMailAPI = new JavaMailAPI(this, mEmail, mSubject, mMessage);
+
+        javaMailAPI.execute();
+    }
+}
---
+++ app/src/main/java/com/morshed/sendemail/Utils.java
@@ -0,0 +1,7 @@
+package com.morshed.sendemail;
+
+public class Utils {
+
+    public static final  String EMAIL = "shawon.swe1@gmail.com";
+    public static final String PASSWORD = "01940263294morshed>";
+}
